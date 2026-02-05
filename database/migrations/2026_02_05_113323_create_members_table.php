d2cf87df9395a871d39a5aeaec61b30511d7e7c4
nazi sharifi <banonazisharifi@gmail.com>
2026-02-05 16:07:51 +0430

    add members table

---
+++ database/migrations/2026_02_05_113323_create_members_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function Symfony\Component\Clock\now;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->text('address');
+            $table->date('membership_date')->default(now());
+            $table->string('whatsApp_number')->nullable();
+            $table->enum('status',['active','inactive'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('members');
+    }
+};
