4bea78ff9dacd28214e1f877d43e853ccda612b0
nazi sharifi <banonazisharifi@gmail.com>
2026-02-19 17:10:09 +0430

    add code request

--- app/Http/Requests/updateBorrowRequest.php
+++ app/Http/Requests/updateBorrowRequest.php
@@ -11,7 +11,7 @@ class updateBorrowRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,12 @@ class updateBorrowRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+               "book_id"=>"nullable|integer",
+        'member_id'=>"nullable|integer",
+        'borrowed_date'=>"nullable|date",
+        'due_date'=> "nullable|date",
+        'returned_date'=>"nullable|date",
+        'status'=> "nullable|string",
         ];
     }
 }
