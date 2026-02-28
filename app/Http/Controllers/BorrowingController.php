a0efafac41ff11e2a31316f927c1799b14c35d68
nazi sharifi <banonazisharifi@gmail.com>
2026-02-28 15:28:26 +0430

    add unic fild

--- app/Http/Controllers/BorrowingController.php
+++ app/Http/Controllers/BorrowingController.php
@@ -57,7 +57,7 @@ class BorrowingController extends Controller
        }
        catch(Exception $err){
         return response()->json([
-            "messege"=> "Somting Went Wrong",
+            "messege"=> $err->getMessage(),
         ]);
        }
     }
