91c62e13b88cf910332d9e452cf19393bb42e851
nazi sharifi <banonazisharifi@gmail.com>
2026-02-28 10:06:44 +0430

    add factory for borrow

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Author;
 use App\Models\Book;
+use App\Models\borrowing;
 use App\Models\member;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -20,7 +21,8 @@ class DatabaseSeeder extends Seeder
     {
         // Book::factory(40)->create();
         // member::factory(40)->create();
-        Author::factory(20)->create();
+        // Author::factory(20)->create();
+        borrowing::factory(50)->create();
         // User::factory(10)->create();
 
         // User::factory()->create([
