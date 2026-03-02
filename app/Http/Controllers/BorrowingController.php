1dca02823e59b0567a844c80cabf889a3e8d7acd
nazi sharifi <banonazisharifi@gmail.com>
2026-03-02 23:40:06 +0430

    new chenges

--- app/Http/Controllers/BorrowingController.php
+++ app/Http/Controllers/BorrowingController.php
@@ -85,5 +85,6 @@ class BorrowingController extends Controller
    public function overdue(){
     $overdueBorrowings = borrowing::with(['books', 'member_borrowing'])
     ->where('status', 'borrowed')
+    ->where('due_date', '<', now())->get();
    }
 }
