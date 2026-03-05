16101a497e8eb118d83f8a271cb0764aec798993
nazi sharifi <banonazisharifi@gmail.com>
2026-03-05 15:35:16 +0430

    add new controller for auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,15 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function Auth(Request $request){
-        $request->validate([
+       $valedatedUser=  $request->validate([
             "name"=> "required|string|min:3|max:25",
             "email"=> "required|email|unique:user,email,id",
-            "password"=> "required|string|min:6|max-25",
+            "password"=> "required|string|min:6|max:25",
+        ]);
+        User::create([
+            "name"=> $valedatedUser->name,
+            
         ])
     }
 }
