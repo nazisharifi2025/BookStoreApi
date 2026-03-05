dc10ca9227a514a510618368b6fbb6c4376d490b
nazi sharifi <banonazisharifi@gmail.com>
2026-03-05 16:22:12 +0430

    add request for auth

--- app/Http/Requests/AuthRequest.php
+++ app/Http/Requests/AuthRequest.php
@@ -22,7 +22,7 @@ class AuthRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "name"=> "required|string|min:3|max:25",
         ];
     }
 }
