b9c2db2c0ce606772e2a215385e663a4d26d4f7e
nazi sharifi <banonazisharifi@gmail.com>
2026-03-05 15:31:27 +0430

    add new controller for auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function Auth(Request $request){
-        
+        $request->validate([
+            "name"=> "required|string|min:3|max:25",
+        ])
     }
 }
