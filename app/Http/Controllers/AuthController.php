f11aa2dce8626a26a28fbe72b80cf978a4ee316b
nazi sharifi <banonazisharifi@gmail.com>
2026-03-05 16:31:08 +0430

    add request for auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends Controller
     public function register(Request $request){
        $valedatedUser=  $request->validate([
             "name"=> "required|string|min:3|max:25",
-            "email"=> "required|email|unique:user,email,id",
+            "email"=> "required|email|unique:user,email",
             "password"=> "required|string|min:6|max:25|confirmed",
         ]);
         $user = User::create([
