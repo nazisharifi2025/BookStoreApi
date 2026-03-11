75f6c94e0f35e6305df3a08a5fc926923404469d
nazi sharifi <banonazisharifi@gmail.com>
2026-03-11 16:40:25 +0430

    add controler versaning

--- app/Http/Controllers/BookV2controller.php
+++ app/Http/Controllers/BookV2controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateBookRequest;
 use App\Http\Resources\BookRsource;
 use App\Models\Book;
 use Exception;
@@ -44,9 +45,22 @@ class BookV2controller extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+   public function store(CreateBookRequest $request)
     {
-        //
+        try{
+            if(!$request->user() || !$request->user()->tokenCan('create-book')) {
+                return response()->json([
+                    "message" => "Unauthorized"
+                ], 303);
+            }
+            $books = Book::create($request->validated());
+        $books->load("authors");
+        return new BookRsource($books);
+        }
+        catch(Exception $err){
+            return $err;
+        }
+
     }
 
     /**
